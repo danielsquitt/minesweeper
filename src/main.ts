d4773a9ce466e30cf72e8e870642e421e34615e3
Daniel Squittieri <danielsquitt@gmail.com>
2022-01-24 18:08:08 +0100

    Set "Hellow world" message in canvas

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,6 @@ window.onload = () => {
   const canvas = document.getElementById("canvas") as HTMLCanvasElement;
   const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
 
-  ctx.fillStyle = "green";
-  ctx.fillRect(10, 10, 100, 100);
+  ctx.font = "30px Arial";
+  ctx.fillText("Hello World", 10, 50);
 };
