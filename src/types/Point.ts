714d3b47aab911b5ccc7a299988b9fb815dbdeb9
Daniel Squittieri <danielsquitt@gmail.com>
2022-01-28 14:25:52 +0100

    Added function typeOfPoint()

--- src/types/Point.ts
+++ src/types/Point.ts
@@ -1,2 +1,7 @@
 // 2D Point definition
 export type Point = { x: number; y: number };
+
+export const typeOfPoint = (x: any) => {
+    let keys = Object.keys(x as Point)
+    return keys[0] == 'x' && keys[1] == 'y' && keys.length == 2;
+}
